4229a2c14eef729f385a65be45bbe6f564fc5ebd
IgiranezaMichel <igiramichael@gmail.com>
2023-12-17 09:10:25 +0200

    Add indexController

--- src/main/java/com/bookambulance/Controller/indexController.java
+++ src/main/java/com/bookambulance/Controller/indexController.java
@@ -2,11 +2,6 @@ package com.bookambulance.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import com.bookambulance.Model.User;
-
 @Controller
 public class indexController {
 public String getHomePage(){
