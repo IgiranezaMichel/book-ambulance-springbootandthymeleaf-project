1d07a7ef91564a75548db8b395d89f5993a0e098
IgiranezaMichel <igiramichael@gmail.com>
2023-12-18 11:54:05 +0200

    Add AdminController class

--- src/main/java/com/bookambulance/Controller/AdminController.java
+++ src/main/java/com/bookambulance/Controller/AdminController.java
@@ -6,17 +6,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
 import com.bookambulance.Model.Hospital;
 import com.bookambulance.Model.User;
 import com.bookambulance.Services.AmbulanceServices;
 import com.bookambulance.Services.HospitalServices;
 import com.bookambulance.Services.UserServices;
 @Controller
+@SessionAttributes("admin")
 @RequestMapping(value = "/admin")
 public class AdminController {
 @Autowired private UserServices userServices;
 @Autowired private HospitalServices hospitalServices;
 @Autowired private AmbulanceServices ambulanceServices;
+private static  User user1;
+public static void setUser(User user){
+user1=user;
+}
 private String message="";
 @GetMapping()
 public String adminPage(Model model){
@@ -24,6 +31,7 @@ public String adminPage(Model model){
     model.addAttribute("userList", userServices.getAllData());
     model.addAttribute("hospitalList", hospitalServices.getAllData());
     model.addAttribute("ambulanceList", ambulanceServices.getAllData());
+    model.addAttribute("admin",user1);
     model.addAttribute("user", new User());
     model.addAttribute("hospital", new Hospital());
     return "admin/index";
