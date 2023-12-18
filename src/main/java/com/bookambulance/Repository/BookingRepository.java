8fd3e7496f32f8bfd42bc778d8cf05699d919472
IgiranezaMichel <igiramichael@gmail.com>
2023-12-18 11:56:36 +0200

    Add BookingRepository class

--- src/main/java/com/bookambulance/Repository/BookingRepository.java
+++ src/main/java/com/bookambulance/Repository/BookingRepository.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.bookambulance.Model.Booking;
+import com.bookambulance.Model.Hospital;
 import com.bookambulance.Model.User;
 public interface BookingRepository extends JpaRepository<Booking,Long>{
 
     List<Booking> findByUser(User user);
+
+    Hospital findByHospital(Hospital hospital);
 
 }
